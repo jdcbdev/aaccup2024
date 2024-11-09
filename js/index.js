1088c5534ede21a882e2378f7e1a032d77f9a7b7
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2024-11-09 21:29:32 +0800

    Fix gallery vid bug

--- js/index.js
+++ js/index.js
@@ -1,33 +1,21 @@
 $(document).ready(function() {
-    lightGallery(document.getElementById('video-gallery'), {
-        selector: '.gallery-img-container',
-        plugins: [lgVideo, lgThumbnail, lgFullscreen],
-        youTubePlayerParams: {
-            controls: 1,
-            mute: 0,
-            autoplay: 1
-        }
-    });
 
-    lightGallery(document.getElementById('video-gallery-presentation'), {
-        selector: '.gallery-img-container',
-        plugins: [lgVideo, lgThumbnail, lgFullscreen],
-        youTubePlayerParams: {
-            controls: 1,
-            mute: 0,
-            autoplay: 1
-        }
-    });
+    function initializeGallery(galleryId) {
+        lightGallery(document.getElementById(galleryId), {
+            selector: '.gallery-img-container',
+            plugins: [lgVideo, lgThumbnail, lgFullscreen],
+            youTubePlayerParams: {
+                controls: 1,
+                mute: 0,
+                autoplay: 1
+            }
+        });
+    }
 
-    lightGallery(document.getElementById('video-gallery-featured-students'), {
-        selector: '.gallery-img-container',
-        plugins: [lgVideo, lgThumbnail, lgFullscreen],
-        youTubePlayerParams: {
-            controls: 1,
-            mute: 0,
-            autoplay: 1
-        }
-    });
+    initializeGallery('video-gallery')
+    initializeGallery('video-gallery-presentation')
+    initializeGallery('video-gallery-featured-students')
+
 
     $('.owl-carousel').owlCarousel({
         loop: true,
